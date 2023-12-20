4277e852996f90e53750229324e5ace8e5b2ca8d
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-21 00:09:21 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -66,7 +66,20 @@ class MyFormat:
     #         with open(file_markdown, 'w', encoding="utf-8") as file:
     #             file.write(contents)
 
-    def basic(path):
+    def basic1(path):
+        with open(path, 'r', encoding="utf-8") as file:
+            contents = file.read()
+
+
+        while '  ' in contents:
+            contents = contents.replace('  ', ' ')
+        contents = '\n'.join(line.strip() for line in contents.split('\n'))
+        while "\n\n\n" in contents:
+            contents = contents.replace("\n\n\n", "\n\n")
+        contents = contents.lstrip('\n')
+        with open(path, 'w', encoding="utf-8") as file:
+            file.write(contents)
+    def basic2(path):
         with open(path, 'r', encoding="utf-8") as file:
             contents = file.read()
 
@@ -81,31 +94,36 @@ class MyFormat:
         with open(path, 'w', encoding="utf-8") as file:
             file.write(contents)
 
-    def latex(contents):
-        latex_files = glob.glob(os.path.join(
-            contents, f'**/*.tex'), recursive=True)
+    def latex(contents): 
+        if os.path.isfile(contents):
+            MyFormat.basic1(contents) 
+        else:
 
-        for latex_file in latex_files:
-            with open(latex_file, 'r', encoding="utf-8") as file:
-                contents = file.read()
 
-            while '  ' in contents:
-                contents = contents.replace('  ', ' ')
+            latex_files = glob.glob(os.path.join(
+                contents, f'**/*.tex'), recursive=True)
 
-            while '( ' in contents:
-                contents = contents.replace('( ', '(')
-            while ' )' in contents:
-                contents = contents.replace(' )', ')')
-            while '[ ' in contents:
-                contents = contents.replace('[ ', '[')
-            while ' ]' in contents:
-                contents = contents.replace(' ]', ']')
-            while '{ ' in contents:
-                contents = contents.replace('{ ', '{')
-            while ' }' in contents:
-                contents = contents.replace(' }', '}')
+            for latex_file in latex_files:
+                with open(latex_file, 'r', encoding="utf-8") as file:
+                    contents = file.read()
 
-            with open(latex_file, 'w', encoding="utf-8") as file:
-                file.write(contents)
+                while '  ' in contents:
+                    contents = contents.replace('  ', ' ')
 
-            MyFormat.basic(latex_file)
+                while '( ' in contents:
+                    contents = contents.replace('( ', '(')
+                while ' )' in contents:
+                    contents = contents.replace(' )', ')')
+                while '[ ' in contents:
+                    contents = contents.replace('[ ', '[')
+                while ' ]' in contents:
+                    contents = contents.replace(' ]', ']')
+                while '{ ' in contents:
+                    contents = contents.replace('{ ', '{')
+                while ' }' in contents:
+                    contents = contents.replace(' }', '}')
+
+                with open(latex_file, 'w', encoding="utf-8") as file:
+                    file.write(contents)
+
+                MyFormat.basic2(latex_file)
--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -7,6 +7,7 @@ message = "VuVanNghia20206205"
 
 git =  os.path.join(os.getcwd(), '../../../')
 contents =  os.path.join(os.getcwd(), '../../../baocao/contents')
+main =  os.path.join(os.getcwd(), '../../../baocao/contents/main')
 # init_path =  os.path.join(os.getcwd(), '../../../baocao/contents/start/init.sty')
 # workspace_path = os.path.join(os.getcwd(), '../../report.code-workspace')
 # gitignore_path = os.path.join(git_path, ".gitignore") 
@@ -16,6 +17,7 @@ MyGit.add()
 MyGit.commit(message)
 
 MyFormat.latex(contents)
+MyFormat.latex(main)
 # MyFormat.markdown(git_path)
 # MyFormat.basic(gitignore_path)
 # MyFormat.basic(init_path)
